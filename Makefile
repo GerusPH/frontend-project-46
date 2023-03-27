6a32c98804ffeba07c781f7791d7a3e482c94533
GerusPH <avn89@bk.ru>
2023-03-27 17:20:54 +0300

    First step

---
+++ Makefile
@@ -0,0 +1,12 @@
+install:
+	npm ci
+
+gendiff -h:
+	node bin/gendiff.js
+
+publish:
+	npm publish --dry-run
+
+lint:
+	npx eslint .
+
---
+++ bin/gendiff.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+program
+.description('Compares two configuration files and shows a difference.')
+.version('0.1.0')
+.option('-h, --help', 'display help for command')
+.parse();
+
+if (program.help) {
+program.help();
+}
