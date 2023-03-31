63660910b11127d8e7a25616523c00a070405fde
GerusPH <avn89@bk.ru>
2023-03-31 17:17:30 +0300

    jest tests fix

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -12,13 +12,13 @@ const __dirname = dirname(__filename);
 const getFixturePath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
 
 const expectedResult = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
- }`;
+ - follow: false
+   host: hexlet.io
+ - proxy: 123.234.53.22
+ - timeout: 50
+ + timeout: 20
+ + verbose: true
+}`;
  
  test('test1', () => {
    expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expectedResult);
